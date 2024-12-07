54e6e8d8c0794ad62ea770a02842a26bdd29a5d9
vanek-198803 <vanek.198803v@gmail.com>
2024-12-07 19:02:35 +0300

    lesson2

---
+++ lesson3/03_lesson/Address.py
@@ -0,0 +1,15 @@
+class Address:
+
+    def __init__(self, index, city, street, house, apartment):
+        self.index = index
+        self.city = city
+        self.street = street
+        self.house = house
+        self.apartment = apartment
+
+    def __str__(self):
+        return f"{self.index}, {self.city}, {self.street}, {self.house}, Apt: {self.apartment}"
+
+
+address = Address("101100", "Белореченск", "Ленина", "5", "8")
+print(address)
