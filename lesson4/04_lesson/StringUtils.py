6b88c08385aed19dcdec168c0fc648e011ba8873
vanek-198803 <vanek.198803v@gmail.com>
2024-12-23 20:19:04 +0300

    lesson2

--- lesson4/04_lesson/StringUtils.py
+++ lesson4/04_lesson/StringUtils.py
@@ -1,13 +1,11 @@
 class StringUtils:
-    """
-    Класс с полезными утилитами для обработки и анализа строк
-    """
+    """ Класс с полезными утилитами
+     для обработки и анализа строк"""
 
     def capitilize(self, string: str) -> str:
-        """
-        Принимает на вход текст, делает первую букву заглавной и возвращает этот же текст
-        Пример: `capitilize("skypro") -> "Skypro"`
-        """
+        """ Принимает на вход текст, делает первую букву
+         заглавной и возвращает этот же текст
+        Пример: `capitilize("skypro") -> "Skypro"` """
         return string.capitalize()
 
     def trim(self, string: str) -> str:
@@ -16,8 +14,12 @@ class StringUtils:
         Пример: `trim("   skypro") -> "skypro"`
         """
         whitespace = " "
+        # Удаляем пробелы в начале
         while string.startswith(whitespace):
             string = string.removeprefix(whitespace)
+        # Удаляем пробелы в конце
+        while string.endswith(whitespace):
+            string = string.removesuffix(whitespace)
         return string
 
     def to_list(self, string: str, delimeter=",") -> list[str]:
@@ -35,14 +37,13 @@ class StringUtils:
         return string.split(delimeter)
 
     def contains(self, string: str, symbol: str) -> bool:
-        """
-        Возвращает `True`, если строка содержит искомый символ и `False` - если нет \n
+        """ Возвращает `True`, если строка содержит искомый
+         символ и `False` - если нет \n
         Параметры: \n
             `string` - строка для обработки \n
             `symbol` - искомый символ \n
         Пример 1: `contains("SkyPro", "S") -> True`
-        Пример 2: `contains("SkyPro", "U") -> False`
-        """
+        Пример 2: `contains("SkyPro", "U") -> False` """
         res = False
         try:
             res = string.index(symbol) > -1
@@ -65,8 +66,9 @@ class StringUtils:
         return string
 
     def starts_with(self, string: str, symbol: str) -> bool:
-        """
-        Возвращает `True`, если строка начинается с заданного символа и `False` - если нет \n
+        """ Возвращает `True`, если строка начинается
+         с заданного символа и `False`
+         - если нет \n
         Параметры: \n
             `string` - строка для обработки \n
             `symbol` - искомый символ \n
@@ -77,7 +79,8 @@ class StringUtils:
 
     def end_with(self, string: str, symbol: str) -> bool:
         """
-        Возвращает `True`, если строка заканчивается заданным символом и `False` - если нет \n
+        Возвращает `True`, если строка заканчивается заданным
+         символом и `False` - если нет \n
         Параметры: \n
             `string` - строка для обработки \n
             `symbol` - искомый символ \n
@@ -101,7 +104,8 @@ class StringUtils:
         Преобразует список элементов в строку с указанным разделителем \n
         Параметры: \n
             `lst` - список элементов \n
-            `joiner` - разделитель элементов в строке. По умолчанию запятая (", ") \n
+            `joiner` - разделитель элементов в строке.
+             По умолчанию запятая (", ") \n
         Пример 1: `list_to_string([1,2,3,4]) -> "1, 2, 3, 4"`
         Пример 2: `list_to_string(["Sky", "Pro"]) -> "Sky, Pro"`
         Пример 3: `list_to_string(["Sky", "Pro"], "-") -> "Sky-Pro"`
