1969a55b37f2ac9fd1a92f63c3f8a983e76d7e3d
vanek-198803 <vanek.198803v@gmail.com>
2025-01-19 11:21:19 +0300

    lesson2

--- lesson6K/06K_lesson/test_01_from.py
+++ lesson6K/06K_lesson/test_01_from.py
@@ -1,35 +1,71 @@
+import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_fill_from():
-    driver = webdriver.Chrome()
+@pytest.fixture
+def driver():
+    driver = (webdriver.Chrome
+              (service=ChromeService(ChromeDriverManager().install())))
+    yield driver
+    driver.quit()
 
-    # Открытие веб-страницы
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+
+def wait_for_element(driver, by, value):
+    return (WebDriverWait(driver, 10)
+            .until(EC.visibility_of_element_located((by, value))))
+
+
+def test_01_form(driver):
+    (driver.get
+     ("https://bonigarcia.dev/selenium-webdriver-java/data-types.html"))
+
     # Заполнение формы
-    driver.find_element(By.NAME, "first-name").send_keys("Иван")
-    driver.find_element(By.NAME, "last-name").send_keys("Петров")
-    driver.find_element(By.NAME, 'address').send_keys("Ленина, 55-3")
-    driver.find_element(By.NAME, 'e-mail').send_keys("test@skypro.com")
-    driver.find_element(By.NAME, 'phone').send_keys("+7985899998787")
-    driver.find_element(By.NAME, 'zip-code').send_keys("")
-    driver.find_element(By.NAME, 'city').send_keys("Москва")
-    driver.find_element(By.NAME, 'country').send_keys("Россия")
-    driver.find_element(By.NAME, 'job-position').send_keys("QA")
-    driver.find_element(By.NAME, 'company').send_keys("SkyPro")
+    field_names = [
+        "first-name", "last-name", "address", "e-mail",
+        "phone", "zip-code", "city", "country",
+        "job-position", "company"
+    ]
 
-    # Нажатие кнопки Submit
-    driver.find_element(By.XPATH, "//button[text()='Submit']").click()
-    # Ожидание, пока элементы не станут доступными
-    (WebDriverWait(driver, 10).
-     until(EC.visibility_of_element_located((By.ID, "zip-code"))))
-    assert "danger" in driver.find_element(By.ID, "zip-code").get_attribute("class")
+    values = [
+        "Иван", "Петров", "Ленина, 55-3", "test@skypro.com",
+        "+7985899998787", "", "Москва", "Россия",
+        "QA", "SkyPro"
+    ]
 
-    assert "success" in driver.find_elements(By.ID, '[firstName, lastName, address, email, phone, city, country, job, company]').get_attribute("class")
-    # не могу понять, как проверить ысе ячейки зеленого цвета
+    for name, value in zip(field_names, values):
+        wait_for_element(driver, By.NAME, name).send_keys(value)
 
+    # Нажимаем кнопку "Submit"
+    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.ID, "company"))
+    )
+
+    assert "danger" in driver.find_element(By.ID, "zip-code").get_attribute(
+        "class"), "Поле Zip code должно быть подсвечено красным"
+
+    fields = [
+        "first-name",
+        "last-name",
+        "address",
+        "e-mail",
+        "phone",
+        "city",
+        "country",
+        "job-position",
+        "company"
+    ]
+    for field_id in fields:
+        driver.find_element(By.ID, field_id)
+        assert "success" in driver.find_element(By.ID, field_id).get_attribute(
+            "class"), f"Поле {field_id} должно быть подсвечено зеленым"
+
+    print("Все проверки пройдены успешно!")
 
     driver.quit()
